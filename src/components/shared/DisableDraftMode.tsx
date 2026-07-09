4967d8f380687ad042b00acdb04eb7329b904222
James Rea <jamesrea.work@gmail.com>
2026-07-09 19:08:47 +0100

    fix(sanity): refresh page immediately when disabling draft mode

--- src/components/shared/DisableDraftMode.tsx
+++ src/components/shared/DisableDraftMode.tsx
@@ -1,30 +1,36 @@
 "use client";
 
-import Link from "next/link";
 import { Ban } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useVisualEditingEnvironment } from "next-sanity/hooks";
 
 export function DisableDraftMode() {
-  
+
+  const router = useRouter();
   const environment = useVisualEditingEnvironment();
 
   if (environment === "presentation-iframe" || environment === "presentation-window") {
     return null;
   };
 
+  async function disableDraftMode() {
+    await fetch("/api/draft-mode/disable");
+    router.refresh();
+  };
+
   return (
-    <Link
-      href="/api/draft-mode/disable"
+    <button
+      onClick={disableDraftMode}
       aria-label="Disable Draft Mode"
-      className="fixed bottom-4 right-4 flex items-center gap-1.5 bg-black text-white px-2.5 py-2 rounded-lg"
+      className="fixed bottom-4 right-4 flex items-center gap-1.5 bg-black text-white px-2.5 py-2 rounded-lg cursor-pointer"
     >
-      <Ban 
-        size={14} 
-        className="text-red-300" 
+      <Ban
+        size={14}
+        className="text-red-300"
       />
       <span className="text-sm font-medium">
         Disable Draft Mode
       </span>
-    </Link>
+    </button>
   );
 };
